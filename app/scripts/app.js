6aab9d957442244dc5d96a9c9a6bf351b3dcf25f
Jack Keller <j3kk3l@gmail.com>
2015-01-24 13:35:04 -0500

    start

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gitLoveApp
+ * @description
+ * # gitLoveApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('gitLoveApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gitLoveApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gitLoveApp
+ */
+angular.module('gitLoveApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.categories = [
+      {'id': 0, 'name': 'Development'},
+      {'id': 1, 'name': 'Design'},
+      {'id': 2, 'name': 'Exercise'},
+      {'id': 3, 'name': 'Humor'}
+    ];
+
+    $scope.bookmarks = [
+      {'id': 0, 'title': 'AngularJS', 'url': 'http://angularjs.org', 'category': 'Development' },
+      {'id': 1, 'title': 'Egghead.io', 'url': 'http://angularjs.org', 'category': 'Development' },
+      {'id': 2, 'title': 'A List Apart', 'url': 'http://alistapart.com/', 'category': 'Design' },
+      {'id': 3, 'title': 'One Page Love', 'url': 'http://onepagelove.com/', 'category': 'Design' },
+      {'id': 4, 'title': 'MobilityWOD', 'url': 'http://www.mobilitywod.com/', 'category': 'Exercise' },
+      {'id': 5, 'title': 'Robb Wolf', 'url': 'http://robbwolf.com/', 'category': 'Exercise' },
+      {'id': 6, 'title': 'Senor Gif', 'url': 'http://memebase.cheezburger.com/senorgif', 'category': 'Humor' },
+      {'id': 7, 'title': 'Wimp', 'url': 'http://wimp.com', 'category': 'Humor' },
+      {'id': 8, 'title': 'Dump', 'url': 'http://dump.com', 'category': 'Humor' }
+    ];
+
+
+
+
+
+  });
